fae936bfdb06b213691d7a76ea8c85b253bd9f06
Sanu-Ranjan <ranjan.code33@gmail.com>
2024-12-08 13:37:53 +0530

    interaction with users with basic alert-prompt-confirm

--- playground.js
+++ playground.js
@@ -1,16 +1,2 @@
-let user = {
-  name: "John",
-  sizes: {
-    height: 182,
-    width: 50,
-  },
-  dis: function () {
-    console.log(this.name);
-  },
-};
-console.log(user.dis());
-let user2 = clone(user);
-console.table(user2);
-function clone(obj) {
-  return structuredClone(obj);
-}
+const userName = prompt("What is your name", "Sanu");
+alert(userName);
